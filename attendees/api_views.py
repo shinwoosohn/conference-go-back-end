748b4d7890eb8bc6ea468a023fd454fd2e441b3b
Edward Sohn <edwardsohn96@gmail.com>
2023-02-06 16:29:00 -0800

    Solutions for Learn JSON Views Assignment

--- attendees/api_views.py
+++ attendees/api_views.py
@@ -23,7 +23,14 @@ def api_list_attendees(request, conference_id):
         ]
     }
     """
-    return JsonResponse({})
+    attendees = [
+        {
+            "name": attendee.name,
+            "href": attendee.get_api_url(),
+        }
+        for attendee in Attendee.objects.filter(conference=conference_id)
+    ]
+    return JsonResponse({"attendees": attendees})
 
 
 def api_show_attendee(request, id):
@@ -46,4 +53,16 @@ def api_show_attendee(request, id):
         }
     }
     """
-    return JsonResponse({})
+    attendee = Attendee.objects.get(id=id)
+    return JsonResponse(
+        {
+            "email": attendee.email,
+            "name": attendee.name,
+            "company_name": attendee.company_name,
+            "created": attendee.created,
+            "conference": {
+                "name": attendee.conference.name,
+                "href": attendee.conference.get_api_url(),
+            },
+        }
+    )
--- events/api_views.py
+++ events/api_views.py
@@ -97,7 +97,16 @@ def api_list_locations(request):
         ]
     }
     """
-    return JsonResponse({})
+    response = []
+    locations = Location.objects.all()
+    for location in locations:
+        response.append(
+            {
+                "name": location.name,
+                "href": location.get_api_url(),
+            }
+        )
+    return JsonResponse({"locations": response})
 
 
 def api_show_location(request, id):
@@ -117,4 +126,14 @@ def api_show_location(request, id):
         "state": the two-letter abbreviation for the state,
     }
     """
-    return JsonResponse({})
+    location = Location.objects.get(id=id)
+    return JsonResponse(
+        {
+            "name": location.name,
+            "city": location.city,
+            "room_count": location.room_count,
+            "created": location.created,
+            "updated": location.updated,
+            "state": location.state.abbreviation,
+        }
+    )
